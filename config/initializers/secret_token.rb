689fb9c61ad174f95bc97e978ccccd5d2943297f
Hozumi Kaneko <hozumi@homiez.net>
2011-12-04 11:25:56 +0900

    FirstCommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Himasche::Application.config.secret_token = 'd62ae765b1dd810a8ed066ba770c12397e70dd81e223ad7c3486303fe7539b434ad0e2ea026d6bd4693754eb2a95b9f457bb25d77d4752f96ae903975f0f5888'
